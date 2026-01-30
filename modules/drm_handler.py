7755ecc501166909da0f590c4997fd80966399da
wahabbhai52 <gouravbhai6267363@gmail.com>
2026-01-30 16:40:24 +0530

    Update drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -315,7 +315,7 @@ async def drm_handler(bot: Client, m: Message):
 
             #elif "d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
             elif "childId" in url and "parentId" in url:
-                url = f"https://anonymouspwplayer-554b25895c1a.herokuapp.com/pw?url={url}&token={pwtoken}"
+                url = f"https://anonymouspwplayerr-c96de7802811.herokuapp.com/pw?url={url}&token={pw_token}"
                                       
             elif 'encrypted.m' in url:
                 appxkey = url.split('*')[1]
